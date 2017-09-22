e362a71ae98f48aa95884f0fea7b731d82833bf7
faisal arkan <faisalarkan21@gmail.com>
2017-09-22 13:55:31 +0700

    final rebuild

---
+++ airlines_clone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="airlines_clone.MvcApplication" Language="C#" %>
---
+++ airlines_clone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(airlines_clone.Startup))]
+namespace airlines_clone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
